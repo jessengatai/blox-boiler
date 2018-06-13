08028bd0270198758f88cc3875037f03ea5c9744
Jesse Ngatai <jessengatai@gmail.com>
2018-06-13 19:40:26 +1000

    overhaul hashes

---
+++ js/_hashes.js
@@ -0,0 +1,20 @@
+console.log('hashes loaded');
+jQuery(document).ready(function($){
+
+  // listen for hash changes
+  $(window).on
+
+  return $(window).trigger(`modal-closed:${modalId}`);
+
+
+  $(document).on('click', function(){
+
+    $('[data-hash="#foo"]').css('background','red');
+
+  });
+
+  // data-hash="#foo"
+  // data-classes-onhash="purple"
+  // data-classes-offhash
+
+});
--- public/js/main.js
+++ public/js/main.js
@@ -13,6 +13,24 @@ var setBloxId = function setBloxId(el) {
 console.log('boxes loaded');
 jQuery(document).ready(function ($) {});
 
+console.log('hashes loaded');
+jQuery(document).ready(function ($) {
+
+  // listen for hash changes
+  $(window).on;
+
+  return $(window).trigger('modal-closed:' + modalId);
+
+  $(document).on('click', function () {
+
+    $('[data-hash="#foo"]').css('background', 'red');
+  });
+
+  // data-hash="#foo"
+  // data-classes-onhash="purple"
+  // data-classes-offhash
+});
+
 console.log('modal loaded');
 jQuery(document).ready(function ($) {
 
